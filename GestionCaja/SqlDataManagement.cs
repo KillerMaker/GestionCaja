8876845ee3d8b7630ab06d859116b5c7e6b26974
KillerMaker <68623767+KillerMaker@users.noreply.github.com>
2021-02-08 09:32:33 -0400

    123

--- GestionCaja/SqlDataManagement.cs
+++ GestionCaja/SqlDataManagement.cs
@@ -43,7 +43,7 @@ namespace GestionCaja
         }
         public SqlDataReader ExecuteReader()
         {
-            //xd
+            //lalalalalalala
             try
             {
                 connection.Open();
