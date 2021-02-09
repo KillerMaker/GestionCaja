734f7b24822fb4b44c1190538c521b537f83415d
KillerMaker <68623767+KillerMaker@users.noreply.github.com>
2021-02-08 21:07:51 -0400

    Organizacion

---
+++ GestionCaja/Entidades/CDocumento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCaja
+{
+    class CDocumento
+    {
+        public readonly int? id;
+        public readonly string descripcion;
+        public readonly string estado;
+    }
+}
---
+++ GestionCaja/Entidades/CServicio.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GestionCaja
+{
+    class CServicio
+    {
+        public readonly int? id;
+        public readonly string tipoServicio;
+        public readonly string fechaServicio;
+        public readonly string descripcion;
+        public readonly string estado;
+    }
+}
