d0beb05425c6a12fc33efee6c646e0c27c2c7a58
AMAURIS GABRIEL ARCHIBALD PÉREZ <20191343@unapec.edu.do>
2021-02-14 09:55:59 -0400

    commit joder

--- GestionCaja/Program.cs
+++ GestionCaja/Program.cs
@@ -18,5 +18,6 @@ namespace GestionCaja
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
         }
+        //Favor ignorar este comentario
     }
 }
